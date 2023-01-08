bac4d68051d1fcd7800a30607d5a3532e333524a
martin <martinhc129@gmail.com>
2023-01-08 17:52:56 -0500

    added code

---
+++ js/code.js
@@ -0,0 +1,147 @@
+// added variales for elements
+var questionsEl = document.getElementById('questions');
+var timerEl = document.getElementById('time');
+var choicesEl = document.getElementById('choices');
+var submitBtn = document.getElementById('submit');
+var startBtn = document.getElementById('start');
+var initialsEl = document.getElementById('initials');
+var feedbackEl = document.getElementById('feedback');
+
+// added variables for timr and current state of quiz 
+var currentQuestionIndex = 0;
+var time = questions.length * 20;
+var timerId;
+
+function starts() {
+    var startScreenEl = document.getElementById("start-screen");
+    startScreenEl.setAttribute('class', 'hide');
+
+    questionsEl.removeAttribute('class');
+
+    timerId = setInterval(clockTick, 1000);
+
+    timerEl.textContent = time;
+
+    getQuestion();
+} 
+
+function getQuestion() {
+    // get question object from array
+    var currentQuestion = questions[currentQuestionIndex];
+  
+    // update title with question
+    var titleEl = document.getElementById('question-title');
+    titleEl.textContent = currentQuestion.title;
+  
+    choicesEl.innerHTML = '';
+  
+    for (var i = 0; i < currentQuestion.choices.length; i++) {
+      var choice = currentQuestion.choices[i];
+      var choiceNode = document.createElement('button');
+      choiceNode.setAttribute('class', 'choice');
+      choiceNode.setAttribute('value', choice);
+  
+      choiceNode.textContent = i + 1 + '. ' + choice;
+  
+      choicesEl.appendChild(choiceNode);
+    }
+  }
+  
+  function questionClick(event) {
+    // get reference to feedback element
+    var feedbackEl = document.getElementById('feedback');
+  
+    var buttonEl = event.target;
+  
+    if (!buttonEl.matches('.choice')) {
+      return;
+    }
+  
+    // check if correct
+    if (buttonEl.value === questions[currentQuestionIndex].answer) {
+      // show "Correct" message and green background
+      feedbackEl.textContent = 'Correct';
+      feedbackEl.style.backgroundColor = 'green';
+    } else {
+      // show "Wrong" message and red background
+      feedbackEl.textContent = 'Wrong';
+      feedbackEl.style.backgroundColor = 'red';
+      // subtract 5 seconds from timer
+      time -= 10;
+    }
+  
+    // // flash right/wrong feedback
+    feedbackEl.style.display = 'block';
+    setTimeout(function () {
+      feedbackEl.style.display = 'none';
+    }, 1000);
+  
+    // next question
+    currentQuestionIndex++;
+  
+    // check if we've run out of questions
+    if (time <= 0 || currentQuestionIndex === questions.length) {
+      quizEnd();
+    } else {
+      getQuestion();
+    }
+  }
+  
+  function quizEnd() {
+    // stop timer
+    clearInterval(timerId);
+
+    // show end screen
+    var endScreenEl = document.getElementById('end-screen');
+    endScreenEl.removeAttribute('class');
+  
+    // show final score
+    var finalScoreEl = document.getElementById('final-score');
+    finalScoreEl.textContent = time;
+  
+    // hide questions section
+    questionsEl.setAttribute('class', 'hide');
+  }
+  
+  function clockTick() {
+    // update time
+    time--;
+    timerEl.textContent = time;
+  
+    // check if user ran out of time
+    if (time <= 0) {
+      quizEnd();
+    }
+  }
+  
+  function saveHighscore() {
+    var initials = initialsEl.value.trim();
+  
+    if (initials !== '') {
+      var highscores =
+        JSON.parse(window.localStorage.getItem('highscores')) || [];
+  
+      var newScore = {
+        score: time,
+        initials: initials,
+      };
+  
+      // save scores 
+      highscores.push(newScore);
+      window.localStorage.setItem('highscores', JSON.stringify(highscores));
+  
+      //open highscores page
+      window.location.href = 'highscores.html';
+    }
+  }
+  
+  function checkForEnter(event) {
+    if (event.key === 'Enter') {
+      saveHighscore();
+    }
+  }
+
+submitBtn.onclick = saveHighscore;
+startBtn.onclick = starts;
+choicesEl.onclick = questionClick;
+initialsEl.onkeyup = checkForEnter;
---
+++ js/questions.js
@@ -0,0 +1,59 @@
+// just took the questions off the internet 
+var questions = [
+{
+    title: 'Which of the following keywords is used to define a variable in Javascript',
+    choices:
+    [
+        'let',
+        'var',
+        'both A and B',
+        'None of the above'
+    ],
+    answer: 'both A and B',
+},
+{
+    title: 'When an operators value is NULL, the typeof returned by the unary operator is',
+    choices: 
+    [
+        'Object',
+        'Boolean',
+        'Undefined',
+        'Integer',
+    ],
+    answer: 'Object',
+},
+{
+    title: 'The full form of CSS is',
+    choices: 
+    [
+        'Colored Special Sheets',
+        'Color and Styler Sheets',
+        'Cascading Style Sheets',
+        'None of the Above',
+    ],
+    answer: 'Cascading Style Sheets',
+},
+{
+    title: 'Setting an inline-block in CSS requires which of the following properties?',
+    choices: 
+    [
+        'Display',
+        'Color',
+        'Block',
+        'Align',
+    ],
+    answer: 'Display',
+},
+{
+    title: ' The correct sequence of HTML tags for starting a webpage is',
+    choices: 
+    [
+        'Head, Title, HTML, body',
+        'HTML, Body, Title, Head',
+        'Body, Title, Head, HTML',
+        'HTML, Head, Title, Body',
+    ],
+    answer: 'HTML, Head, Title, Body',
+},
+
+];
